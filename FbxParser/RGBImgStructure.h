41f97b08669128b4fdc2cca1322c3f5a4f786059
Larry King <1412857955@qq.com>
2018-01-02 22:09:35 +0800

    add texture loading function

---
+++ FbxParser/RGBImgStructure.h
@@ -0,0 +1,10 @@
+#ifndef RGBIMGSTRUCTURE_H
+#define RGBIMGSTRUCTURE_H
+
+typedef struct __RGBImgStructure{
+	unsigned long width;
+	unsigned long height;
+	unsigned char *data;
+}RGBImgStructure;
+
+#endif
